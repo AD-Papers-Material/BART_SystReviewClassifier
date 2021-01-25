c2232c42f3fc08ebdfe0626681fe08e2f7d11c04
Angelo D'Ambrosio <bakaburg1@gmail.com>
2021-01-25 18:57:24 +0100

    feat: add tool to access content in dynamic websites

--- Setup.R
+++ Setup.R
@@ -43,6 +43,62 @@ percent <- function(x) {
 	})
 }
 
+# Tool to grab XHR messages from dynamic websites
+get_website_resources <- function(url, url_filter = '.*', type_filter = '.*', wait_for = 20, n_of_resources = NULL, interactive = F) {
+
+	crrri::perform_with_chrome(function(client) {
+		Fetch <- client$Fetch
+		Page <- client$Page
+
+		if (interactive) client$inspect()
+
+		out <- new.env()
+
+		out$results <- list()
+		out$resolve_function <- NULL
+
+		out$pr <- promises::promise(function(resolve, reject) {
+			out$resolve_function <- resolve
+
+			Fetch$enable(patterns = list(list(urlPattern="*", requestStage="Response"))) %...>% {
+				Fetch$requestPaused(callback = function(params) {
+
+					if (str_detect(params$request$url, url_filter) & str_detect(params$resourceType, type_filter)) {
+
+						Fetch$getResponseBody(requestId = params$requestId) %...>% {
+							resp <- .
+
+							if (resp$body != '') {
+								if (resp$base64Encoded) resp$body = jsonlite::base64_dec(resp$body) %>% rawToChar()
+
+								body <- list(list(
+									url = params$request$url,
+									response = resp
+								)) %>% setNames(params$requestId)
+
+								#str(body)
+
+								out$results <- append(out$results, body)
+
+								if (!is.null(n_of_resources) & length(out$results) >= n_of_resources) out$resolve_function(out$results)
+							}
+
+						}
+					}
+
+					Fetch$continueRequest(requestId = params$requestId)
+				})
+			} %...>% {
+				Page$navigate(url)
+			} %>% crrri::wait(wait_for) %>%
+				then(~ out$resolve_function(out$results))
+
+		})
+
+		out$pr$then(function(x) x)
+	}, timeouts = max(wait_for + 3, 30), cleaning_timeout = max(wait_for + 3, 30))
+}
+
 
 # A file path friendly lubridate::now()
 safe_now <- function() str_replace_all(lubridate::now(), c(' ' = 'T', ':' = '.'))
@@ -329,8 +385,6 @@ search_pubmed <- function(query, year_query = NULL, additional_fields = NULL,
 	records
 
 }
-
-
 
 read_bib_files <- function(files) {
 
